df9da179ef5dc52e9e03e7ae23e8392e8bfcb915
winnyDiese <winnydiese@gmail.com>
2023-12-21 02:33:24 +0100

    Creating Object of POO

---
+++ Person.ts
@@ -0,0 +1,26 @@
+
+function f(param:any) {
+    return function name(target:any,props:any) {
+        console.log("Deco "+param)
+    }
+}
+
+class Person{
+    private _name:String
+
+    constructor(a:String){
+        console.log('constructor (a:string)')
+        this._name=a
+    }
+
+    get name():String{
+        return this._name
+    }
+
+    @f("un param")
+    direBonjour(){
+        console.log("Bonjour je m'appelle : "+this._name)
+    }
+}
+
+let a = new Person('Robert')
--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+"use strict";
 function greeter(person) {
     return "Hello " + person;
 }
